2fd6ed3258aa1b6ceec94c113dcb3acfb5703020
Noli Briones <124874418+nuleeeee@users.noreply.github.com>
2025-02-23 14:35:27 +0800

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -13,8 +13,8 @@ def create_app():
 	app = Flask(__name__)
 	app.config["DEBUG"] = True
 	app.config['SECRET_KEY'] = 'nols'
-	# app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:nols@localhost/flask'
+	app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+	# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:nols@localhost/flask'
 	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 	db.init_app(app)
